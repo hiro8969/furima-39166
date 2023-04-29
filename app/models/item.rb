2761a6efa0fd49fd36630b7eedaa6055e88d54e6
hiro8969 <129718390+hiro8969@users.noreply.github.com>
2023-04-29 18:11:49 +0900

    配送料カテゴリー実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,16 @@ class Item < ApplicationRecord
 
 extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_time
+
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank"} 
+  validates :shipping_time_id, numericality: { other_than: 1 , message: "can't be blank"} 
+
 end
