fb1d3a65f4e5889e4a189ed5155ee616a1aab2d9
hiro8969 <129718390+hiro8969@users.noreply.github.com>
2023-05-01 14:45:51 +0900

    再修正完了

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -34,28 +34,32 @@ RSpec.describe Item, type: :model do
       end
       it 'カテゴリーが空の場合は出品できない' do
         @item.category_id = nil
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it '商品の状態が空の場合は出品できない' do
         @item.condition_id = nil
+        @item.condition_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
       it '配送料の負担が空の場合は出品できない' do
-      @item.shipping_cost_id = nil
+        @item.shipping_cost_id = nil
+        @item.shipping_cost_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping cost can't be blank")
       end
       it '発送元の地域が空の場合は出品できない' do
         @item.prefecture_id = nil
+        @item.prefecture_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
-      end
+      end      
       it '発送までの日数が空の場合は出品できない' do
         @item.shipping_time_id = nil
+        @item.shipping_time_id = 1
         @item.valid?
-        binding.pry
         expect(@item.errors.full_messages).to include("Shipping time can't be blank")
       end
       it '価格が空では保存できない' do
